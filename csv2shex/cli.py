0e8a80b856ff8c00d76e75fdb2e4c48f52bcbf2b
Tom Baker <tom@tombaker.org>
2020-07-12 19:23:04 +0200

    csv2shex parse works now

--- csv2shex/cli.py
+++ csv2shex/cli.py
@@ -6,7 +6,7 @@ from dataclasses import asdict
 import click
 from .prefixes import write_starter_prefixfile, PREFIXFILE_NAME
 from .mkstatements import csvreader, list_statements
-from .mkshapes import list_shapes
+from .mkshapes import pprint_shapes, list_shapes
 
 # pylint: disable=unused-argument
 #         During development, unused arguments here.
@@ -29,34 +29,9 @@ def parse(config, csvfile):
 
     statements = list_statements(csvreader(csvfile))
     shapes = list_shapes(statements)
-    for item in shapes:
-        item = asdict(item)
-        print(f"Shape")
-        print(f"    start:    {item['start']}")
-        print(f"    shape_id: {item['shape_id']}")
-        if item['shape_label']:
-            print(f"    shape_label: {item['shape_label']}")
-        for row in item['shape_statements']:
-            print(f"    Statement")
-            print(f"        prop_id:          {row['prop_id']}")
-            if row['prop_label']:
-                print(f"        prop_label:       {row['prop_label']}")
-            if row['mand']:
-                print(f"        mand:             {row['mand']}")
-            if row['repeat']:
-                print(f"        repeat:           {row['repeat']}")
-            if row['value_type']:
-                print(f"        value_type:       {row['value_type']}")
-            if row['value_datatype']:
-                print(f"        value_datatype:   {row['value_datatype']}")
-            if row['constraint_value']:
-                print(f"        constraint_value: {row['constraint_value']}")
-            if row['constraint_type']:
-                print(f"        constraint_type:  {row['constraint_type']}")
-            if row['shape_ref']:
-                print(f"        shape_ref:        {row['shape_ref']}")
-            if row['annot']:
-                print(f"        annot:            {row['annot']}")
+    pprint_output = pprint_shapes(shapes)
+    for line in pprint_output.splitlines():
+        print(line)
 
 
 @cli.command()
