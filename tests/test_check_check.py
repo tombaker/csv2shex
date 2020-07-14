ea0c7968c72f4b1cb3441e2c4b35e52ee58d8d2f
Tom Baker <tom@tombaker.org>
2020-07-14 15:19:43 +0200

    try to fix test_check_check.py

--- tests/test_check_check.py
+++ tests/test_check_check.py
@@ -1,24 +1,26 @@
 """Check CSV file structure for anomalies"""
 
 
-import os
 import pytest
-from pathlib import Path
-
-# from csv2shex.check import check
-from csv2shex.mkstatements import csvreader, list_statements
+from csv2shex.mkstatements import Statement
 
 
 @pytest.mark.skip
-def test_check(tmp_path):
-    """Check CSV file structure for anomalies"""
-    os.chdir(tmp_path)
-    csvfile = Path(tmp_path).joinpath("some.csv")
-    csvfile.write_text(
-        (
-            "prop_id,value_type,constraint_type,constraint_value\n"
-            "wdt:P31,URI,URIStem,wd:\n"
-        )
+def test_check():
+    """Check CSV file structure for anomalies."""
+    statement = Statement(
+        start=True,
+        shape_id="@default",
+        shape_label=None,
+        prop_id="wdt:P31",
+        prop_label=None,
+        mand=None,
+        repeat=None,
+        value_type="URI",
+        value_datatype=None,
+        constraint_value="wd:",
+        constraint_type="URIStem",
+        shape_ref=None,
+        annot=None,
     )
-    for statement in list_statements(csvreader(csvfile)):
-        assert statement._is_uristem_used_correctly()
+    assert statement._is_uristem_used_correctly()
