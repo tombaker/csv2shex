6b7a5da1b5c46369655bf6acd0a203b471565c87
Tom Baker <tom@tombaker.org>
2020-07-14 18:18:55 +0200

    absolute_minimal_profile.csv

--- csv2shex/constants.py
+++ csv2shex/constants.py
@@ -18,17 +18,17 @@ STATEMENT_KEYS = [
 
 PREFIXFILE_NAME = "prefixes.yml"
 
-PREFIXFILE_CONTENT = (
-    "prefixes:\n"
-    "    dc: http://purl.org/dc/elements/1.1/\n"
-    "    dcat: http://www.w3.org/ns/dcat\n"
-    "    dct: http://purl.org/dc/terms/\n"
-    "    dcterms: http://purl.org/dc/terms/\n"
-    "    foaf: http://xmlns.com/foaf/0.1/\n"
-    "    rdf: http://www.w3.org/1999/02/22-rdf-syntax-ns#\n"
-    "    rdfs: http://www.w3.org/2000/01/rdf-schema#\n"
-    "    skos: http://www.w3.org/2004/02/skos/core#\n"
-    "    skosxl: http://www.w3.org/2008/05/skos-xl#\n"
-    "    sx: http://www.w3.org/ns/shex\n"
-    "    xsd: http://www.w3.org/2001/XMLSchema\n"
-)
+PREFIXFILE_CONTENT = """\
+prefixes:
+    dc: http://purl.org/dc/elements/1.1/
+    dcat: http://www.w3.org/ns/dcat
+    dct: http://purl.org/dc/terms/
+    dcterms: http://purl.org/dc/terms/
+    foaf: http://xmlns.com/foaf/0.1/
+    rdf: http://www.w3.org/1999/02/22-rdf-syntax-ns#
+    rdfs: http://www.w3.org/2000/01/rdf-schema#
+    skos: http://www.w3.org/2004/02/skos/core#
+    skosxl: http://www.w3.org/2008/05/skos-xl#
+    sx: http://www.w3.org/ns/shex
+    xsd: http://www.w3.org/2001/XMLSchema
+"""
