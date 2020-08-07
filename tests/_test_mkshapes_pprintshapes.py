ed6a21d43dc3eb0f07f768a2ba7e6aa09e1fbff5
Tom Baker <tom@tombaker.org>
2020-08-07 10:09:43 +0200

    Minor edit to commented-out test.

--- tests/_test_mkshapes_pprintshapes.py
+++ tests/_test_mkshapes_pprintshapes.py
@@ -40,8 +40,7 @@ def test_pprint_shapes():
             ],
         )
     ]
-    expected = dedent(
-        """\
+    expected = dedent("""\
     Shape
         start: True
         shape_id: @a
@@ -55,38 +54,3 @@ def test_pprint_shapes():
     )
     print(pprint_shapes(as_input))
     assert pprint_shapes(as_input) == expected
-
-
-as_input = [
-    Shape(
-        start=True,
-        shape_id="@a",
-        shape_label=None,
-        shape_statements=[
-            {
-                "prop_id": "dct:creator",
-                "value_type": "URI",
-                "prop_label": None,
-                "mand": None,
-                "repeat": None,
-                "value_datatype": None,
-                "constraint_value": None,
-                "constraint_type": None,
-                "shape_ref": None,
-                "annot": None,
-            },
-            {
-                "prop_id": "dct:date",
-                "value_type": "String",
-                "prop_label": None,
-                "mand": None,
-                "repeat": None,
-                "value_datatype": None,
-                "constraint_value": None,
-                "constraint_type": None,
-                "shape_ref": None,
-                "annot": None,
-            },
-        ],
-    )
-]
