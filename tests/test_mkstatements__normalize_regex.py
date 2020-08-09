ada4226bdf3488f7d4dd3c9c47a05df02a28dbae
Tom Baker <tom@tombaker.org>
2020-08-09 08:53:18 +0200

    more Regex tests - 54 passing, 3 skipped, pylint/flake8 happy

--- tests/test_mkstatements__normalize_regex.py
+++ tests/test_mkstatements__normalize_regex.py
@@ -8,7 +8,9 @@ from csv2shex.mkstatements import Statement
 def test_mkstatements_validate_regex():
     """@@@"""
     stat = Statement(
-        prop_id=":status", constraint_value="approved_*", constraint_type="Regex",
+        prop_id=":status", 
+        constraint_value="approved_*", 
+        constraint_type="Regex",
     )
     stat._normalize_regex()
     assert stat.constraint_value == re.compile("approved_*")
@@ -17,7 +19,43 @@ def test_mkstatements_validate_regex():
 def test_mkstatements_validate_regex_does_not_compile():
     """@@@"""
     stat = Statement(
-        prop_id=":status", constraint_value="approved_(*", constraint_type="Regex",
+        prop_id=":status", 
+        constraint_value="approved_(*", 
+        constraint_type="Regex",
     )
     stat._normalize_regex()
     assert not stat.constraint_value
+
+
+def test_mkstatements_validate_regex_none_value_ignored():
+    """@@@"""
+    stat = Statement(
+        prop_id=":status", 
+        constraint_type="Regex",
+    )
+    stat._normalize_regex()
+    assert not stat.constraint_value
+
+
+def test_mkstatements_validate_regex_forward_slashes_are_part():
+    """@@@"""
+    stat = Statement(
+        prop_id=":status", 
+        constraint_value="/approved_*/", 
+        constraint_type="Regex",
+    )
+    stat._normalize_regex()
+    assert stat.constraint_value == re.compile("/approved_*/")
+
+
+def test_mkstatements_validate_regex_blanks_are_part():
+    """@@@"""
+    stat = Statement(
+        prop_id=":status", 
+        constraint_value="^2020 August",
+        constraint_type="Regex",
+    )
+    stat._normalize_regex()
+    assert stat.constraint_value == re.compile("^2020 August")
+    
+
