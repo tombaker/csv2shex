2d5c39d6b65d873ab2243df0b07d6da44811d83e
Tom Baker <tom@tombaker.org>
2020-08-17 11:17:08 +0200

    Add to "Questions" in shapetoshex.py

--- csv2shex/shapetoshex.py
+++ csv2shex/shapetoshex.py
@@ -18,14 +18,21 @@ from csv2shex.mkshapes import Shape as CSVShape, Statement as CSVStatement
 # Questions:
 # 1) is s.shape_id always present or do we have to supply it?
 #    A: According to mkstatements, will always be present.
+#    Q: Current default is "default" - is there a conventional name to use?
 # 2) What is the shape label used for?
-#    A: probably nothing except documentation.  Should we embed it as a comment?
+#    A: probably nothing except documentation.
+#    Q: Should we embed it as a comment in ShEx?
+#    A: Yes - or optionally generate UI (form-building) statements (eg sh:group)?
 # 3) Are multiple shape statements considered ShapeOr or ShapeAnd?
 #    We are assuming AND for the time being
 # 4) We assign prop_label to statement.id, but but there may be labels
 #    that don't fit the id value space
 # 5) Need to define how "prefixed URI string" maps to IRIREF
 #    -- probably need a sub function
+#    A: Maybe use prefix declarations to expand all prefixed URI strings.
+#       - as defined in csv2shex.config.PREFIXES (built-in)
+#       - as defined (or pointed to) in a csv2shex config file?
+#       - as defined in a config file as per a CLI option?
 
 
 def statementtonodeconstraint(statement: CSVStatement) -> Optional[shapeExpr]:
