417e68d072bb2943ec7b7a8198a646d46a9360ee
Tom Baker <tom@tombaker.org>
2020-10-12 12:11:19 +0200

    merge headers:csv2shex/csvreader.py

---
+++ csv2shex/csvreader.py
@@ -0,0 +1,39 @@
+"""Reader of CSV files."""
+
+
+import re
+import csv
+from pathlib import Path
+import ruamel.yaml as yaml
+# from .utils import is_uri, is_valid_uri_or_prefixed_uri
+
+# pylint: disable=no-self-use,too-many-branches,too-many-instance-attributes
+# => self-use: for now...
+# => too-many-branches: a matter of taste?
+# => too-many-instance-attributes: disagree!
+
+
+def csvreader(csvfile):
+    """Read CSV file and return list of dicts, one dict per CSV row."""
+    rows_odict = csv.DictReader(Path(csvfile).open(newline="", encoding="utf-8-sig"))
+    csvrow_dicts_list = [dict(r) for r in rows_odict]
+    return csvrow_dicts_list
+
+
+def normalize_csvheaders(csvrow_dicts_list):
+    """return csvrow_dicts_list_normalized """
+    return yaml.safe_load(csvrow_dicts_list)
+
+
+def _snakify(csvheader=None):
+    """Convert spaces, dashes, and camelcase to lowercase snake case."""
+    csvheader = re.sub('-', '_', csvheader)
+    csvheader = re.sub(' ', '_', csvheader)
+    new = []
+    new.extend(csvheader[0])
+    for char in csvheader[1:]:
+        if char.isupper():
+            new.extend('_')
+        new.extend(char)
+    csvheader = ''.join(new)
+    return csvheader.lower()
