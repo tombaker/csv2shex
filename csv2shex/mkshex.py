85ea35cafd61c24285de064d5a3e4a3c31c07701
Tom Baker <tom@tombaker.org>
2020-10-27 16:56:27 +0100

    resolve questions in mkshex.py

--- csv2shex/mkshex.py
+++ csv2shex/mkshex.py
@@ -15,32 +15,6 @@ from ShExJSG.ShExJ import (
 from csv2shex.mkshapes import Shape as CSVShape, Statement as CSVStatement
 
 
-# Questions:
-# 1) is s.shapeID always present or do we have to supply it?
-#    Normalized CSVStatement objects are always supposed to have 
-#    a 'shapeID' object. If 'shapeID' is None, 'shapeID' of previous 
-#    statement is assigned.
-#    Q: Current default is "default" - is there a conventional name to use?
-# 2) What is the shape label used for?
-#    A: shapeID is the shape identifier; shapeLabel is a human-readable label,
-#    only for documentation.
-#    Q: Should we embed it as a comment in ShEx?
-#    A: Yes - or optionally generate UI (form-building) statements (eg sh:group)?
-#    Let's ignore these annotations for now...
-# 3) Are multiple shape statements considered ShapeOr or ShapeAnd?
-#    We are assuming AND for the time being.
-#    Q: Do not understand ShapeOr...
-# 4) We assign propertyLabel to statement.id, but but there may be labels
-#    that don't fit the id value space
-#    A: propertyID is the identifier; propertyLabel is just a human-readable label
-# 5) Need to define how "prefixed URI string" maps to IRIREF
-#    -- probably need a sub function
-#    A: Maybe use prefix declarations to expand all prefixed URI strings.
-#       - as defined in csv2shex.config.PREFIXES (built-in)
-#       - as defined (or pointed to) in a csv2shex config file?
-#       - as defined in a config file as per a CLI option?
-
-
 def statement_to_node_constraint(statement: CSVStatement) -> Optional[shapeExpr]:
     """ Generate a node constraint from statement if necessary """
     rval = None
@@ -71,7 +45,6 @@ def add_statement(shape: Shape, statement: CSVStatement) -> None:
     #                          "TripleConstraint",
     #                          typing.Union[str, str]]]
     ts = TripleConstraint(
-        id=statement.propertyLabel,     # what is this?
         predicate=IRIREF(statement.propertyID),
         min=1 if statement.mandatory else 0,
         max=-1 if statement.repeatable else 1,
