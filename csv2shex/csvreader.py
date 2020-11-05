7995254b39c13be475dee22000f3f43a96801f8d
Tom Baker <tom@tombaker.org>
2020-11-05 15:45:36 +0100

    fix pylint R1721: Unnecessary use of a comprehension (unnecessary-comprehension)

--- csv2shex/csvreader.py
+++ csv2shex/csvreader.py
@@ -16,7 +16,7 @@ from .exceptions import CsvError
 def csvreader(csvfile):
     """Read CSV file and return list of dicts, one dict per CSV row."""
     csv_dictreader_obj = csv.DictReader(Path(csvfile).open(newline="", encoding="utf-8-sig"))
-    csvrow_dicts_list = [r for r in csv_dictreader_obj]
+    csvrow_dicts_list = list(csv_dictreader_obj)
     if "propertyID" not in list(csvrow_dicts_list[0].keys()):
         raise CsvError("To be valid, DCAP CSV must have a 'propertyID' column header.")
     return csvrow_dicts_list
