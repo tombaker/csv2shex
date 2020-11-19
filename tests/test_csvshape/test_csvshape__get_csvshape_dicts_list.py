beb9134c060ef9868b27596a4d1d04cb5df704aa
Tom Baker <tom@tombaker.org>
2020-11-19 10:34:29 +0100

    condensing test_csvshape/ tests

--- tests/test_csvshape/test_csvshape__get_csvshape_dicts_list.py
+++ tests/test_csvshape/test_csvshape__get_csvshape_dicts_list.py
@@ -9,15 +9,17 @@ from csv2shex.csvshape import get_csvshape_dicts_list
 
 
 def test_get_csvshape_dicts_list_one_shape():
-    """Turn list of CSVRow objects into list with one CSVShape."""
+    """Get csvshape dict with one shape from list of CSVRow object(s)."""
     csvrow_objs_list = [
         CSVRow(shapeID=":a", propertyID="dct:creator"),
-        CSVRow(shapeID=":a", propertyID="dct:date"),
     ]
 
     csvshape_dicts_list = get_csvshape_dicts_list(csvrow_objs_list)
     assert csvshape_dicts_list[0]["shapeID"] == ":a"
-
+    assert csvshape_dicts_list[0]["start"] == True
+    assert csvshape_dicts_list[0]["pvdicts_list"]
+    assert csvshape_dicts_list[0]["pvdicts_list"][0]["propertyID"] == "dct:creator"
+    assert csvshape_dicts_list[0]["pvdicts_list"][0]["mandatory"] == False
     expected_csvshape_dicts_list = [
         {
             "shapeID": ":a",
@@ -37,318 +39,41 @@ def test_get_csvshape_dicts_list_one_shape():
                     "valueShape": None,
                     "note": None,
                 },
-                {
-                    "propertyID": "dct:date",
-                    "propertyLabel": None,
-                    "mandatory": False,
-                    "repeatable": False,
-                    "valueNodeType": None,
-                    "valueDataType": None,
-                    "valueConstraint": None,
-                    "valueConstraintType": None,
-                    "valueShape": None,
-                    "note": None,
-                },
             ],
         }
     ]
-    assert get_csvshape_dicts_list(csvrow_objs_list) == expected_csvshape_dicts_list
+    assert csvshape_dicts_list == expected_csvshape_dicts_list
 
 
 def test_get_csvshape_dicts_list_two_shapes():
-    """Turn list of CSVRow objects into list with two CSVShapes.
-    @TODO handling of shapeClosed"""
+    """Get csvshape dict with two shapes from list of CSVRow objects."""
     csvrow_objs_list = [
         CSVRow(shapeID=":a", propertyID="dct:creator"),
         CSVRow(shapeID=":a", propertyID="dct:date"),
         CSVRow(shapeID=":b", propertyID="foaf:name"),
     ]
-
-    expected_csvshape_dicts_list = [
-        {
-            "shapeID": ":a",
-            "shapeLabel": None,
-            "start": True,
-            "shapeClosed": None,
-            "pvdicts_list": [
-                {
-                    "propertyID": "dct:creator",
-                    "mandatory": False,
-                    "note": None,
-                    "propertyLabel": None,
-                    "repeatable": False,
-                    "valueConstraint": None,
-                    "valueConstraintType": None,
-                    "valueDataType": None,
-                    "valueNodeType": None,
-                    "valueShape": None,
-                },
-                {
-                    "propertyID": "dct:date",
-                    "mandatory": False,
-                    "note": None,
-                    "propertyLabel": None,
-                    "repeatable": False,
-                    "valueConstraint": None,
-                    "valueConstraintType": None,
-                    "valueDataType": None,
-                    "valueNodeType": None,
-                    "valueShape": None,
-                },
-            ],
-        },
-        {
-            "shapeID": ":b",
-            "shapeLabel": None,
-            "start": False,
-            "shapeClosed": None,
-            "pvdicts_list": [
-                {
-                    "propertyID": "foaf:name",
-                    "mandatory": False,
-                    "note": None,
-                    "propertyLabel": None,
-                    "repeatable": False,
-                    "valueConstraint": None,
-                    "valueConstraintType": None,
-                    "valueDataType": None,
-                    "valueNodeType": None,
-                    "valueShape": None,
-                }
-            ],
-        },
-    ]
-    assert len(get_csvshape_dicts_list(csvrow_objs_list)) == len(expected_csvshape_dicts_list)
-    assert (
-        get_csvshape_dicts_list(csvrow_objs_list)[0]["pvdicts_list"]
-        == expected_csvshape_dicts_list[0]["pvdicts_list"]
-    )
-    assert get_csvshape_dicts_list(csvrow_objs_list) == expected_csvshape_dicts_list
+    csvshape_dicts_list = get_csvshape_dicts_list(csvrow_objs_list)
+    assert csvshape_dicts_list[1]["shapeID"] == ":b"
+    assert csvshape_dicts_list[1]["start"] == False
+    assert csvshape_dicts_list[1]["pvdicts_list"][0]["propertyID"] == "foaf:name"
 
 
-def test_get_csvshape_dicts_list_one_shape_and_shapeLabel():
-    """One CSVShape with shape label."""
+def test_get_csvshape_dicts_list_takes_label_for_first_shapeid_encountered():
+    """Csvshape dict takes shapeLabel for any new shapeID encountered."""
     csvrow_objs_list = [
-        CSVRow(
-            shapeID=":a",
-            shapeLabel="Author",
-            propertyID="dct:creator",
-            valueNodeType="URI",
-        ),
-        CSVRow(
-            shapeID=":a",
-            shapeLabel="Author",
-            propertyID="dct:date",
-            valueNodeType="String",
-        ),
+        CSVRow(shapeID=":a", shapeLabel="Author", propertyID="dct:creator"),
+        CSVRow(shapeID=":a", shapeLabel="Creator", propertyID="dct:date"),
     ]
-
-    csvshape_dicts_list = [
-        {
-            "shapeID": ":a",
-            "shapeLabel": "Author",
-            "start": True,
-            "shapeClosed": None,
-            "pvdicts_list": [
-                {
-                    "propertyID": "dct:creator",
-                    "valueNodeType": "URI",
-                    "valueDataType": None,
-                    "propertyLabel": None,
-                    "mandatory": False,
-                    "repeatable": False,
-                    "valueConstraint": None,
-                    "valueConstraintType": None,
-                    "valueShape": None,
-                    "note": None,
-                },
-                {
-                    "propertyID": "dct:date",
-                    "valueNodeType": "String",
-                    "valueDataType": None,
-                    "propertyLabel": None,
-                    "mandatory": False,
-                    "repeatable": False,
-                    "valueConstraint": None,
-                    "valueConstraintType": None,
-                    "valueShape": None,
-                    "note": None,
-                },
-            ],
-        }
-    ]
-    assert get_csvshape_dicts_list(csvrow_objs_list) == csvshape_dicts_list
+    csvshape_dicts_list = get_csvshape_dicts_list(csvrow_objs_list)
+    assert csvshape_dicts_list[0]["shapeLabel"] == "Author"
 
 
-def test_get_csvshape_dicts_list_two_shapes_assign_start_to_first():
-    """First shape created is marked as the 'start' shape."""
+def test_get_csvshape_dicts_list_assigns_start_to_first_shape_created():
+    """First shape created is marked as 'start' shape."""
     csvrow_objs_list = [
-        CSVRow(shapeID=":a", propertyID=":prop1"),
-        CSVRow(shapeID=":a", propertyID=":prop2"),
-        CSVRow(shapeID=":b", propertyID=":prop3"),
+        CSVRow(shapeID=":a", propertyID=":propa"),
+        CSVRow(shapeID=":b", propertyID=":propb"),
     ]
-    csvshape_dicts_list = [
-        {
-            "shapeID": ":a",
-            "shapeLabel": None,
-            "start": True,
-            "shapeClosed": None,
-            "pvdicts_list": [
-                {
-                    "propertyID": ":prop1",
-                    "valueNodeType": None,
-                    "valueDataType": None,
-                    "propertyLabel": None,
-                    "mandatory": False,
-                    "repeatable": False,
-                    "valueConstraint": None,
-                    "valueConstraintType": None,
-                    "valueShape": None,
-                    "note": None,
-                },
-                {
-                    "propertyID": ":prop2",
-                    "valueNodeType": None,
-                    "valueDataType": None,
-                    "propertyLabel": None,
-                    "mandatory": False,
-                    "repeatable": False,
-                    "valueConstraint": None,
-                    "valueConstraintType": None,
-                    "valueShape": None,
-                    "note": None,
-                },
-            ],
-        },
-        {
-            "shapeID": ":b",
-            "shapeLabel": None,
-            "start": False,
-            "shapeClosed": None,
-            "pvdicts_list": [
-                {
-                    "propertyID": ":prop3",
-                    "valueNodeType": None,
-                    "valueDataType": None,
-                    "propertyLabel": None,
-                    "mandatory": False,
-                    "repeatable": False,
-                    "valueConstraint": None,
-                    "valueConstraintType": None,
-                    "valueShape": None,
-                    "note": None,
-                }
-            ],
-        },
-    ]
-    assert get_csvshape_dicts_list(csvrow_objs_list) == csvshape_dicts_list
-
-
-def test_listshapes_one_shape_for_shex_example():
-    """Turn list of CSVRow objects into list with one CSVShape."""
-    csvrow_objs_list = [
-        CSVRow(
-            shapeID="http://example.org/myshape",
-            propertyID="http://purl.org/dc/terms/title",
-            mandatory=True,
-            repeatable=False,
-            valueNodeType="literal",
-            valueDataType=None,
-            valueConstraint=None,
-            valueConstraintType=None,
-            valueShape=None,
-        ),
-        CSVRow(
-            shapeID="http://example.org/myshape",
-            propertyID="http://purl.org/dc/terms/description",
-            mandatory=False,
-            repeatable=False,
-            valueNodeType=None,
-            valueDataType="http://www.w3.org/2001/XMLSchema#string",
-            valueConstraint=None,
-            valueConstraintType=None,
-            valueShape=None,
-        ),
-        CSVRow(
-            shapeID="http://example.org/myshape",
-            propertyID="http://purl.org/dc/terms/subject",
-            mandatory=False,
-            repeatable=False,
-            valueNodeType=None,
-            valueDataType=None,
-            valueConstraint="http://lod.nal.usda.gov/nalt/",
-            valueConstraintType="IriStem",
-            valueShape=None,
-        ),
-        CSVRow(
-            shapeID="http://example.org/myshape",
-            propertyID="http://purl.org/dc/terms/creator",
-            mandatory=False,
-            repeatable=False,
-            valueNodeType=None,
-            valueDataType=None,
-            valueConstraint=None,
-            valueConstraintType=None,
-            valueShape="http://example.org/mycreator",
-        ),
-    ]
-
-    one_shape_with_csvrows_list = [
-        {
-            "shapeID": "http://example.org/myshape",
-            "shapeLabel": None,
-            "start": True,
-            "shapeClosed": None,
-            "pvdicts_list": [
-                {
-                    "propertyID": "http://purl.org/dc/terms/title",
-                    "propertyLabel": None,
-                    "mandatory": True,
-                    "repeatable": False,
-                    "valueNodeType": "literal",
-                    "valueDataType": None,
-                    "valueConstraint": None,
-                    "valueConstraintType": None,
-                    "valueShape": None,
-                    "note": None,
-                },
-                {
-                    "propertyID": "http://purl.org/dc/terms/description",
-                    "propertyLabel": None,
-                    "mandatory": False,
-                    "repeatable": False,
-                    "valueNodeType": None,
-                    "valueDataType": "http://www.w3.org/2001/XMLSchema#string",
-                    "valueConstraint": None,
-                    "valueConstraintType": None,
-                    "valueShape": None,
-                    "note": None,
-                },
-                {
-                    "propertyID": "http://purl.org/dc/terms/subject",
-                    "propertyLabel": None,
-                    "mandatory": False,
-                    "repeatable": False,
-                    "valueNodeType": None,
-                    "valueDataType": None,
-                    "valueConstraint": "http://lod.nal.usda.gov/nalt/",
-                    "valueConstraintType": "IriStem",
-                    "valueShape": None,
-                    "note": None,
-                },
-                {
-                    "propertyID": "http://purl.org/dc/terms/creator",
-                    "propertyLabel": None,
-                    "mandatory": False,
-                    "repeatable": False,
-                    "valueNodeType": None,
-                    "valueDataType": None,
-                    "valueConstraint": None,
-                    "valueConstraintType": None,
-                    "valueShape": "http://example.org/mycreator",
-                    "note": None,
-                },
-            ],
-        }
-    ]
-    assert get_csvshape_dicts_list(csvrow_objs_list) == one_shape_with_csvrows_list
+    csvshape_dicts_list = get_csvshape_dicts_list(csvrow_objs_list)
+    assert csvshape_dicts_list[0]["start"] == True
+    assert csvshape_dicts_list[1]["start"] == False
