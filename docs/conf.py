a113ec97f9eae5b7d133749023cac1c89f192641
Tom Baker <tom@tombaker.org>
2021-07-25 15:24:16 +0200

    docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,6 @@ extensions = [
     "sphinx.ext.autodoc", 
     "sphinx.ext.napoleon", 
     "sphinx_rtd_theme",
-    "versionwarning.extension",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
