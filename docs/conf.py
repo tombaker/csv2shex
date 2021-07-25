7ac4fd613adda34e92a269bec93cc01cf3508d7b
Tom Baker <tom@tombaker.org>
2021-07-25 15:02:49 +0200

    conf.py - 0.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ copyright = "2020, Tom Baker"
 author = "Tom Baker"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.1"
+release = "0.3.0"
 
 # -- General configuration ---------------------------------------------------
 
